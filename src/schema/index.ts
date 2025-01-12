77bfbca1110719c2207991482e54595cacf0f51d
hrkmtsmt <hrkmtsmt@icloud.com>
2025-01-12 19:25:34 +0900

    🔧 [FIX]: 投稿テーブルのカラムにレコードがインサートされた日と記事が投稿された日付を加えた

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,10 @@
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const posts = sqliteTable('posts', {
   id: text('id').primaryKey().notNull(),
   title: text('title').notNull(),
   url: text('url').notNull(),
-  media: text('media').notNull(),
+  media: text('media', { enum: ['Zenn', 'Qiita', 'Sizu'] }).notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+  publishedAt: integer('published_at', { mode: 'timestamp' }).notNull(),
 });
