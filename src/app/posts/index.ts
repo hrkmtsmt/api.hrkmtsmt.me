9b7196ae19036c7fb556c264e1fc19a8609973e8
hrkmtsmt <hrkmtsmt@icloud.com>
2025-01-12 23:52:34 +0900

    👍 [FEATURE]: Postsテーブルに記事データを挿入するスケジューラーを作成

--- src/app/posts/index.ts
+++ src/app/posts/index.ts
@@ -1,63 +1,68 @@
 import { Hono } from 'hono';
 import { drizzle } from 'drizzle-orm/d1';
-import { Api, toTimestamp } from '@src/modules';
+import { Api } from '@src/modules';
 import * as s from '@src/schema';
 import { Env } from '@src/types';
-import { ListResponse, Zenn, Qiita, Sizu } from './types';
+import { BlankSchema } from 'hono/types';
+import { Zenn, Qiita, Sizu } from './types';
 
-export const posts = new Hono<Env>();
-
-posts.get('/posts', async (c) => {
-  try {
+export const posts = new Hono<Env, BlankSchema, '/'>()
+  .get('/posts', async (c) => {
+    try {
+      const db = drizzle(c.env.DB);
+      return c.json(await db.select().from(s.posts), 200);
+    } catch (error: unknown) {
+      return c.json({ message: 'Failed to fetch posts.' }, 500);
+    }
+  })
+  .put('/posts:bulk', async (c) => {
     const api = new Api(c.env);
-    const secret = JSON.parse(c.req.queries('secret')?.at(0) ?? 'false') as boolean;
 
-    const [z, q, s] = await Promise.all([
+    const [zenn, qiita, sizu] = await Promise.all([
       api.zenn.get<Zenn>('/articles?username=hrkmtsmt'),
       api.qiita.get<Qiita>('/authenticated_user/items'),
-      secret ? api.sizu.get<Sizu>('/posts') : undefined,
+      api.sizu.get<Sizu>('/posts'),
     ]);
 
-    const zenn = z.articles.map<ListResponse[number]>((p) => {
+    const now = new Date();
+
+    const p1 = zenn.articles.map((p) => {
       return {
-        id: toTimestamp(p.publishedAt),
-        media: 'zenn',
+        id: p.id,
+        media: 'Zenn' as const,
         title: p.title,
         url: `${c.env.ZENN_BASE_URL}${p.path}`,
-        createdAt: p.publishedAt,
-        updatedAt: p.bodyUpdatedAt,
+        createdAt: now,
+        publishedAt: new Date(p.bodyUpdatedAt),
       };
     });
 
-    const qiita = q.map<ListResponse[number]>((p) => {
+    const p2 = qiita.map((p) => {
       return {
-        id: toTimestamp(p.createdAt),
-        media: 'qiita',
+        id: p.id,
+        media: 'Qiita' as const,
         title: p.title,
         url: p.url,
-        createdAt: p.createdAt,
-        updatedAt: p.updatedAt,
+        createdAt: now,
+        publishedAt: new Date(p.updatedAt),
       };
     });
 
-    const sizu =
-      s?.posts
-        .filter((p) => p.visibility === 'ANYONE')
-        .map<ListResponse[number]>((p) => {
-          return {
-            id: toTimestamp(p.createdAt),
-            media: 'sizu',
-            title: p.title,
-            url: `${c.env.SIZU_BASE_URL}/${p.slug}`,
-            createdAt: p.createdAt,
-            updatedAt: p.updatedAt,
-          };
-        }) ?? [];
+    const p3 = sizu.posts
+      .filter((p) => p.visibility === 'ANYONE')
+      .map((p) => {
+        return {
+          id: p.slug,
+          media: 'Sizu' as const,
+          title: p.title,
+          url: `${c.env.SIZU_BASE_URL}/${p.slug}`,
+          createdAt: now,
+          publishedAt: new Date(p.updatedAt),
+        };
+      });
 
-    const data: ListResponse = [...zenn, ...qiita, ...sizu].sort((a, b) => b.id - a.id);
+    const db = drizzle(c.env.DB);
+    db.insert(s.posts).values([...p1, ...p2, ...p3]);
 
-    return c.json(data, 200);
-  } catch (error: unknown) {
-    return c.json({ message: 'Failed to fetch data from external service API.' }, 500);
-  }
-});
+    return c.json(null);
+  });
