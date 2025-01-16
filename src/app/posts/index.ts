2f69c6313325dd231c91082c350f3d88ede71ccc
hrkmtsmt <hrkmtsmt@icloud.com>
2025-01-17 00:47:23 +0900

    🔨 [REFACTOR]: /posts:bulkの処理をscheduledに移動

--- src/app/posts/index.ts
+++ src/app/posts/index.ts
@@ -1,75 +1,17 @@
 import { Hono } from 'hono';
 import { drizzle } from 'drizzle-orm/d1';
-import { Api, Logger } from '@src/modules';
+import { Logger } from '@src/modules';
 import * as s from '@src/schema';
 import { Env } from '@src/types';
 import { BlankSchema } from 'hono/types';
-import { Zenn, Qiita, Sizu } from './types';
 import { HTTPException } from 'hono/http-exception';
 
-export const posts = new Hono<Env, BlankSchema, '/'>()
-  .get('/posts', async (c) => {
-    try {
-      const db = drizzle(c.env.DB);
-      return c.json(await db.select().from(s.posts), 200);
-    } catch (error: unknown) {
-      Logger.error(error);
-      throw new HTTPException(500, { message: 'Failed to fetch posts.' });
-    }
-  })
-  .put('/posts:bulk', async (c) => {
-    try {
-      const api = new Api(c.env);
-
-      const [zenn, qiita, sizu] = await Promise.all([
-        api.zenn.get<Zenn>('/articles?username=hrkmtsmt'),
-        api.qiita.get<Qiita>('/authenticated_user/items'),
-        api.sizu.get<Sizu>('/posts'),
-      ]);
-
-      const now = new Date();
-
-      const p1 = zenn.articles.map<typeof s.posts.$inferInsert>((p) => {
-        return {
-          id: p.slug,
-          media: 'Zenn' as const,
-          title: p.title,
-          url: `${c.env.ZENN_BASE_URL}${p.path}`,
-          createdAt: now,
-          publishedAt: new Date(p.bodyUpdatedAt),
-        };
-      });
-
-      const p2 = qiita.map<typeof s.posts.$inferInsert>((p) => {
-        return {
-          id: p.id,
-          media: 'Qiita' as const,
-          title: p.title,
-          url: p.url,
-          createdAt: now,
-          publishedAt: new Date(p.updatedAt),
-        };
-      });
-
-      const p3 = sizu.posts
-        .filter((p) => p.visibility === 'ANYONE')
-        .map<typeof s.posts.$inferInsert>((p) => {
-          return {
-            id: p.slug,
-            media: 'Sizu' as const,
-            title: p.title,
-            url: `${c.env.SIZU_BASE_URL}/hrkmtsmt/posts/${p.slug}`,
-            createdAt: now,
-            publishedAt: new Date(p.updatedAt),
-          };
-        });
-
-      const db = drizzle(c.env.DB);
-      await db.insert(s.posts).values([...p1, ...p2, ...p3]);
-
-      return c.json(null);
-    } catch (error: unknown) {
-      Logger.error(error);
-      throw new HTTPException(500, { message: 'Failed to insert posts.' });
-    }
-  });
+export const posts = new Hono<Env, BlankSchema, '/'>().get('/posts', async (c) => {
+  try {
+    const db = drizzle(c.env.DB);
+    return c.json(await db.select().from(s.posts), 200);
+  } catch (error: unknown) {
+    Logger.error(error);
+    throw new HTTPException(500, { message: 'Failed to fetch posts.' });
+  }
+});
--- src/scheduled.ts
+++ src/scheduled.ts
@@ -1,18 +1,66 @@
-import { hc } from 'hono/client';
-import { toBasicAuth } from './modules';
-import type { AppType } from './main';
+import { drizzle } from 'drizzle-orm/d1';
+import { HTTPException } from 'hono/http-exception';
+import { Api, Logger } from './modules';
+import * as schemas from './schema';
 import type { Env } from './types';
+import type { Qiita, Sizu, Zenn } from './app/posts/types';
 
 export const scheduled: ExportedHandlerScheduledHandler<Env['Bindings']> = async (_, env, context) => {
   context.waitUntil(
     (async () => {
       try {
-        console.log(_, env);
-        const client = hc<AppType>(env.APP_URL);
-        const headers = { Authorization: toBasicAuth(env.SECRET_BASIC_AUTH_USERNAME, env.SECRET_BASIC_AUTH_PASWORD) };
-        await client['posts:bulk'].$put({}, { headers });
+        const api = new Api(env);
+
+        const [zenn, qiita, sizu] = await Promise.all([
+          api.zenn.get<Zenn>('/articles?username=hrkmtsmt'),
+          api.qiita.get<Qiita>('/authenticated_user/items'),
+          api.sizu.get<Sizu>('/posts'),
+        ]);
+
+        const now = new Date();
+
+        const p1 = zenn.articles.map<typeof schemas.posts.$inferInsert>((p) => {
+          return {
+            id: p.slug,
+            media: 'Zenn' as const,
+            title: p.title,
+            url: `${env.ZENN_BASE_URL}${p.path}`,
+            createdAt: now,
+            publishedAt: new Date(p.bodyUpdatedAt),
+          };
+        });
+
+        const p2 = qiita.map<typeof schemas.posts.$inferInsert>((p) => {
+          return {
+            id: p.id,
+            media: 'Qiita' as const,
+            title: p.title,
+            url: p.url,
+            createdAt: now,
+            publishedAt: new Date(p.updatedAt),
+          };
+        });
+
+        const p3 = sizu.posts
+          .filter((p) => p.visibility === 'ANYONE')
+          .map<typeof schemas.posts.$inferInsert>((p) => {
+            return {
+              id: p.slug,
+              media: 'Sizu' as const,
+              title: p.title,
+              url: `${env.SIZU_BASE_URL}/hrkmtsmt/posts/${p.slug}`,
+              createdAt: now,
+              publishedAt: new Date(p.updatedAt),
+            };
+          });
+
+        console.log([...p1, ...p2, ...p3]);
+
+        const db = drizzle(env.DB);
+        await db.insert(schemas.posts).values([...p1, ...p2, ...p3]);
       } catch (error: unknown) {
-        console.error(error);
+        Logger.error(error);
+        throw new HTTPException(500, { message: 'Failed to insert posts.' });
       }
     })()
   );
