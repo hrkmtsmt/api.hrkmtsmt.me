6bf50b78fc3ae96c0032c5cff988aa20d59fb1c3
hrkmtsmt <hrkmtsmt@icloud.com>
2025-01-18 00:41:55 +0900

    🔧 [FIX]: GET /postsでクエリパラメータにsecretがない場合はしずかなインターネットの投稿を含めないようにした

--- src/app/posts/index.ts
+++ src/app/posts/index.ts
@@ -9,7 +9,19 @@ import { HTTPException } from 'hono/http-exception';
 export const posts = new Hono<Env, BlankSchema, '/'>().get('/posts', async (c) => {
   try {
     const db = drizzle(c.env.DB);
-    return c.json(await db.select().from(s.posts), 200);
+    const result = await db.select().from(s.posts);
+    const secret = (p: (typeof result)[number]) => {
+      if (Boolean(c.req.query('secret'))) {
+        return true;
+      }
+
+      return p.media !== 'sizu';
+    };
+    const response = [...result]
+      .filter(secret)
+      .sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime());
+
+    return c.json(response, 200);
   } catch (error: unknown) {
     Logger.error(error);
     throw new HTTPException(500, { message: 'Failed to fetch posts.' });
